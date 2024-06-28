5b4d06e715bda9440a5748cbe02a28624476d5e5
wassimnsiri <wassim.nsiri@esprit.tn>
2024-06-28 14:01:37 +0100

    fixed commande

--- src/pages/Gerant_layout/mescomande.tsx
+++ src/pages/Gerant_layout/mescomande.tsx
@@ -3,28 +3,50 @@ import React, { useEffect, useState } from 'react';
 import { Container, Grid, Card, CardContent, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Chip } from '@mui/material';
 import axios from 'axios';
 import { Commande } from '../../components/models/Commande';
+import User from '../../components/models/User';
+import { fetchUserData1 } from '../../network/user_services';
 
 const Mescommande: React.FC = () => {
   // Initialize state with correct type
   const [commandes, setCommandes] = useState<Commande[]>([]);
-
+  const [userData, setUserData] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
-    // Fetch commandes from API
-    const fetchCommandes = async () => {
+    const fetchData = async () => {
       try {
-        // Adjust API endpoint as necessary
-        const response = await axios.get<Commande[]>('http://localhost:3030/commande/getcommande/6647f9f6420f66a0d5de1fdd');
-        // Update state with fetched commandes
-        setCommandes(response.data);
+        const userData = await fetchUserData1();
+        setUserData(userData);
       } catch (error) {
-        console.error('Error fetching commandes:', error);
+        console.error('Error fetching user data:', error);
+      } finally {
+        setLoading(false);
       }
     };
 
-    // Fetch commandes on component mount
-    fetchCommandes();
+    fetchData();
   }, []);
 
+  useEffect(() => {
+    // Fetch commandes from API only if userData is available
+    const fetchCommandes = async () => {
+      if (userData?._id) {
+        try {
+          const response = await axios.get<Commande[]>(`http://localhost:3030/commande/getcommande/${userData._id}`);
+          // Update state with fetched commandes
+          setCommandes(response.data);
+        } catch (error) {
+          console.error('Error fetching commandes:', error);
+        }
+      }
+    };
+
+    // Fetch commandes when userData changes
+    fetchCommandes();
+  }, [userData]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
   return (
     <Container>
       <Typography variant="h4" gutterBottom>
